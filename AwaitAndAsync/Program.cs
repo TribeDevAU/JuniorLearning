4836fa2c737bd1a7b5076822ea1099f945bbd099
nahidtribedev <83621077+nahidtribedev@users.noreply.github.com>
2021-05-25 13:39:26 +1000

    how Await and async works

---
+++ AwaitAndAsync/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AwaitAndAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintName();
+            PrintSum(1, 2);
+            Console.WriteLine("Third line");
+            Console.WriteLine("Fourth Line");
+            Console.ReadLine();
+        }
+
+
+
+
+
+        static void PrintName()
+        {
+            Console.WriteLine("Nahid Evan");
+        }
+
+
+        static async Task PrintSum(int a, int b)
+        {
+
+            await Task.Run(()=>Console.WriteLine("Sum : " + (a + b)));
+
+        }
+    }
+
+}
